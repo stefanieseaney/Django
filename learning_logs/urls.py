99a333dd93602a05905a074082e4ae06a1b558e4
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-04-20 20:20:21 -0500

    Update typos in html files.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 app_name = "learning_logs"
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
+    path("topics/<int:topic_id>", views.topic, name="topic"),
+    path("new_topic/", views.new_topic, name="new_topic"),
+]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -6,3 +9,30 @@ from django.shortcuts import render
 def index(request):
     """The home page for Learning Log."""
     return render(request, "learning_logs/index.html")
+
+
+def topics(request):
+    topics = Topic.objects.order_by("date_added")
+    context = {"topics": topics}
+    return render(request, "learning_logs/topics.html", context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by("-date_added")
+    context = {"topic": topic, "entries": entries}
+    return render(request, "learning_logs/topic.html", context)
+
+
+def new_topic(request):
+    if request.method != "POST":
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("learning_logs:topics")
+
+    context = {"form": form}
+    return render(request, "learning_logs/new_topic.html", context)
