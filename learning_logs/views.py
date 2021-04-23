4cf932abedb082c4590beb1cecc87cf2f7ec02db
Stefanie Seaney <stefanie.seaney@gmail.com>
2021-04-23 14:11:33 -0500

    Created new topics and entries.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -36,3 +36,21 @@ def new_topic(request):
 
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != "POST":
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            form.save()
+            return redirect("learning_logs:topic", topic_id=topic_id)
+
+    context = {"form": form, "topic": topic}
+    return render(request, "learning_logs/new_entry.html", context)
